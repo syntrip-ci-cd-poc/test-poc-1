b342ca5303f1f880237563f1627c2f701b585a04
Rogier van der Heide <r.vanderheide@wearetriple.com>
2018-12-06 15:46:23 +0100

    Update

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!doctype html>
 
-echo "Hello World"
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>CI/CD Demo</title>
 
-?>
+  <style>
+    h1 {
+        left: 0;
+        line-height: 200px;
+        margin-top: -100px;
+        position: absolute;
+        text-align: center;
+        font: 45px arial, sans-serif;
+        font-weight: bold;
+        top: 50%;
+        width: 100%;
+    }
+  </style>
+</head>
+
+<body>
+    <h1><?php echo "Hello World" ?></h1>
+</body>
+</html>
